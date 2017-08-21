d5e72360865841ed1badcd27b01da67468b07d42
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-21 22:31:26 +0200

    routes refine

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
 
-
-  get '/dashboard', to: "users#dashboard"
-
-  resources :users
-  resources :records
-  resources :wants
-  resources :findings
-
   devise_for :users
-  root to: 'pages#home'
+  resources :records, only: [:index, :show]
 
+  root to: 'users#dashboard'
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
