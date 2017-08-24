b2bc4019175cecb308628e7b808c36cf194422d2
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-24 15:35:58 +0200

    fix wantlist

--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,6 @@ Rails.application.routes.draw do
   get 'discogs/:id' => 'discogs#show', :constraints  => {:id => /.+\.\w{3,4}/}
   resources :discogs do
     collection do
-      get :authenticate
-      get :callback
-      get :whoami
       get :add_want
       get :edit_want
       get :remove_want
