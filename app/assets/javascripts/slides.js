cc47b953d1faf193e49d2d1f2abebb848eef2428
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-24 13:36:31 +0200

    slider refect

--- app/assets/javascripts/slides.js
+++ app/assets/javascripts/slides.js
@@ -1,6 +1,11 @@
 const flkty = new Flickity('.Slider', {
   cellSelector: '.Slide',
   setGallerySize: false,
+  cellAlign: 'left',
+  pageArrows: false,
   pageDots: false,
+  pauseAutoPlayOnHover: false,
+  autoPlay: 1500,
+  prevNextButtons: false,
   wrapAround: true
-})
+});
