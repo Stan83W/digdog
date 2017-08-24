e395f06efbeb06c76273c77134e2432296a8f27f
Sophia Wushanley <swushanley@gmail.com>
2017-08-24 13:45:09 +0200

    started seed faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+covers = ["https://images-na.ssl-images-amazon.com/images/I/71NyDOQ%2BgoL._SL1500_.jpg", "https://images-na.ssl-images-amazon.com/images/I/51byL4ZNcuL._SY300_QL70_.jpg", "http://www.feelnumb.com/wp-content/uploads/2012/07/elvis_presley_elvis_presley_album_cover_Fort_Homer_Hesterly_Armory_tampa_florida.jpg", "https://pxhst.co/avaxhome/14/83/00378314.jpeg", "https://img.discogs.com/_2N9cQVfe6Cel_t3SGK5o9Di0qc=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-2700028-1372205195-2327.jpeg.jpg", "https://images-na.ssl-images-amazon.com/images/I/81Ut0SKKVhL._SL1500_.jpg"]
+markets = ["Ebay", "PriceMinister", "Le bon coin", "Craigslist"]
+times = ["2:05 am", "6:27 pm", "10:22 pm", "1:51 am", "3:44 pm", "11:38 am", "7:11 pm", "6:32 am"]
+
+find_1 = Record.create!
