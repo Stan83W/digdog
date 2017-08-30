5ecc4c345803ea84e6c5d8a131991e013e1ca87b
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-30 18:33:59 +0200

    ajax ok

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -1,13 +1,17 @@
 class WantsController < ApplicationController
   def create
     @user = current_user
-    @wants = Want.where(user_id: @user.id)
     @want = Want.new
     @record = Record.find(params[:record_id])
     @want.record = @record
     @want.user_id = @user.id
+    @wants = Want.where(user_id: @user.id)
+    @status = 'none'
     if @wants.count < 10
       if @want.save
+      	@status = 'wanted'
+      	findings = Finding.where(record_id: @record.id)
+      	@status = 'found' if !findings.nil? && !findings.empty?
         respond_to do |format|
           format.html { redirect_to wantlist_discogs_path }
           format.js  # <-- will render `app/views/wants/create.js.erb`
