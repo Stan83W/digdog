5500ac8b70eea02836341959ddaf3a44b5f37aed
Tristan Bagot <tristan.bagot@gmail.com>
2017-08-30 17:08:48 +0200

    create.js

--- app/controllers/wants_controller.rb
+++ app/controllers/wants_controller.rb
@@ -3,7 +3,8 @@ class WantsController < ApplicationController
     @user = current_user
     @wants = Want.where(user_id: @user.id)
     @want = Want.new
-    @want.record_id = params[:record_id]
+    @record = Record.find(params[:record_id])
+    @want.record = @record
     @want.user_id = @user.id
     if @wants.count < 10
       if @want.save
@@ -17,7 +18,8 @@ class WantsController < ApplicationController
 
   def destroy
     @wants = Want.where(user_id: current_user.id)
-    @want = @wants.find_by(record_id: params[:record_id])
+    @record = Record.find(params[:record_id])
+    @want = @wants.find_by(record_id: @record.id)
     if @want.destroy
       respond_to do |format|
         format.html { redirect_to wantlist_discogs_path }
